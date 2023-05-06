1e2624d00d004a4a0a50284119bf62efcd5a31a6
Kjetil Andresen <kjetil.andrese@gmail.com>
2023-05-06 15:37:59 +0200

    [extension] Add crates plugin

--- lua/custom/plugins.lua
+++ lua/custom/plugins.lua
@@ -11,7 +11,6 @@ local plugins = {
     "neovim/nvim-lspconfig",
     config = function ()
       require "plugins.configs.lspconfig"
-      require "custom.configs.lspconfig"
     end,
   },
   {
@@ -34,6 +33,24 @@ local plugins = {
   },
   {
     "mfussenegger/nvim-dap"
+  },
+  {
+    "saecki/crates.nvim",
+    dependencies = "hrsh7th/nvim-cmp",
+    ft = {"rust", "toml"},
+    config = function (_, opts)
+      local crates = require("crates")
+      crates.setup(opts)
+      crates.show()
+    end,
+  },
+  {
+    "hrsh7th/nvim-cmp",
+    opts = function ()
+      local M = require("plugins.configs.cmp")
+      table.insert(M.sources, {name = "crates"})
+      return M
+    end
   },
 }
 return plugins
